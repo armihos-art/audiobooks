6c1fa0a47ddaab7e57302be9f32d5c4f786090ca
armihos-art <104677895+armihos-art@users.noreply.github.com>
2022-05-02 19:04:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function createTrackItem(index,name,duration){
 var listAudio = [
   {
     name:"Чехов А. П. - Смерть чиновника",
-    file:"../audiobooks/1.mp3",
+    file:"audiobooks/1.mp3",
     duration:"00:28",
     image: "../images/bg1.jpg"
   },
