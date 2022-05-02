ea99d7a024a8a08a1cab2bae7edc37c33ca7653b
armihos-art <104677895+armihos-art@users.noreply.github.com>
2022-05-02 19:05:56 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,13 +39,13 @@ var listAudio = [
     name:"Чехов А. П. - Лошадиные зубы",
     file:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
     duration:"05:53",
-    image: "../images/bg2.jpg"
+    image: "images/bg2.jpg"
   },
   {
     name:"Чехов А. П. - Дачники",
     file:"../audiobooks/3.mp3",
     duration:"00:28",
-    image: "../images/bg3.jpg"
+    image: "images/bg3.jpg"
   }
 ]
 
